7ca616d5f647f39101b93ca10ec14ec1b68b10da
yomZsamora <samora.y@adzumi.co.ke>
2018-06-05 23:11:25 +0300

    Font added successfully.

--- app/src/main/java/com/adzumi/thelookbook/MainActivity.java
+++ app/src/main/java/com/adzumi/thelookbook/MainActivity.java
@@ -1,13 +1,22 @@
 package com.adzumi.thelookbook;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
+        Typeface openSansFont = Typeface.createFromAsset(getAssets(), "fonts/opensans_italic.ttf");
+        welcomeTextView.setTypeface(openSansFont);
     }
 }
