1a0892cc52ccb9acae5d617908db2f1c3255edc5
yomZsamora <samora.y@adzumi.co.ke>
2018-06-05 16:33:37 +0300

    Search view added to  App bar.

--- app/src/main/java/com/adzumi/thelookbook/HomeActivity.java
+++ app/src/main/java/com/adzumi/thelookbook/HomeActivity.java
@@ -2,6 +2,8 @@ package com.adzumi.thelookbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
