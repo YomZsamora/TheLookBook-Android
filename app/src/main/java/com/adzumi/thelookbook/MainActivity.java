011da9a5f7bff62b093cfb8fcd2bc9c100d501fa
yomZsamora <samora.y@adzumi.co.ke>
2018-06-05 14:46:41 +0300

    Added intent to navigate from MainActivity to HomeActivity.

--- app/src/main/java/com/adzumi/thelookbook/MainActivity.java
+++ app/src/main/java/com/adzumi/thelookbook/MainActivity.java
@@ -1,14 +1,19 @@
 package com.adzumi.thelookbook;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView welcomeTextView;
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
         Typeface openSansFont = Typeface.createFromAsset(getAssets(), "fonts/opensans_bold.ttf");
         welcomeTextView.setTypeface(openSansFont);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
