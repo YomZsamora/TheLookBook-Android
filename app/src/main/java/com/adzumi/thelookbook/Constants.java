360639bd0fc1c47b2e31bd125fd56b733da11ebf
yomZsamora <samora.y@adzumi.co.ke>
2018-06-12 11:13:45 +0300

    Added COnstants.java

---
+++ app/src/main/java/com/adzumi/thelookbook/Constants.java
@@ -0,0 +1,9 @@
+package com.adzumi.thelookbook;
+
+public class Constants {
+    public static final String BASE_URL = "https://www.goodreads.com/search/index.xml";
+    public static final String API_PARAMETER = "key";
+    public static final String GOODREADS_API_KEY = BuildConfig.GOODREADS_API_KEY;
+    public static final String QUERY_PARAMETER = "q";
+    public static final String QUERY = "stephen king";
+}
