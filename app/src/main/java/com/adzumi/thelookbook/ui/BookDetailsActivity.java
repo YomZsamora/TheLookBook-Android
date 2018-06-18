25bae005fc2b99a1d44dee30ade5aa7a984edf50
yomZsamora <samora.y@adzumi.co.ke>
2018-06-18 10:43:01 +0300

    Created BookDetailsActivity.

---
+++ app/src/main/java/com/adzumi/thelookbook/ui/BookDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.adzumi.thelookbook.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_details);
+    }
+}
