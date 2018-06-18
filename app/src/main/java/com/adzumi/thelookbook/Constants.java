09a80f828ea5bf3644c1ec1b98fe697f529ede56
yomZsamora <samora.y@adzumi.co.ke>
2018-06-18 20:17:08 +0300

    Saving to Shared Preferences.

--- app/src/main/java/com/adzumi/thelookbook/Constants.java
+++ app/src/main/java/com/adzumi/thelookbook/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String API_PARAMETER = "key";
     public static final String GOODREADS_API_KEY = BuildConfig.GOODREADS_API_KEY;
     public static final String QUERY_PARAMETER = "q";
-    public static final String QUERY = "stephen king";
+    public static final String PREFERENCES_NAME_KEY = "username";
 }
