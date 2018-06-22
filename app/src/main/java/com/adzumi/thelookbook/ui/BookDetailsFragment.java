49289daf585adf2cfb3d510ff42e7ec94b56ef5a
yomZsamora <samora.y@adzumi.co.ke>
2018-06-22 08:26:59 +0300

    Before Models Refactor.

--- app/src/main/java/com/adzumi/thelookbook/ui/BookDetailsFragment.java
+++ app/src/main/java/com/adzumi/thelookbook/ui/BookDetailsFragment.java
@@ -6,25 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.adzumi.thelookbook.R;
+import com.adzumi.thelookbook.models.Work;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class BookDetailsFragment extends Fragment {
 
+    @BindView(R.id.bookImageDetail) ImageView mBookImageDetail;
+    @BindView(R.id.bookTitleDetail) TextView mBookTitleDetail;
+    @BindView(R.id.bookDescriptionDetail) TextView mBookDescriptionDetail;
+    @BindView(R.id.bookAuthorName) TextView mBookAuthorName;
 
-    public BookDetailsFragment() {
-        // Required empty public constructor
+    private Work mWork;
+
+    public static BookDetailsFragment newInstance(Work work) {
+        BookDetailsFragment bookDetailFragment = new BookDetailsFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("work", Parcels.wrap(work));
+        bookDetailFragment.setArguments(args);
+        return bookDetailFragment;
     }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWork = Parcels.unwrap(getArguments().getParcelable("work"));
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_book_details, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mWork.getBestBook().getImageUrl()).into(mBookImageDetail);
+
+        mBookTitleDetail.setText(mWork.getBestBook().getTitle());
+
+        return view;
+    }
 }
