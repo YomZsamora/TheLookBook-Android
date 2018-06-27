e3dfb528de362ecd69800b990849e7b384c66489
yomZsamora <samora.y@adzumi.co.ke>
2018-06-27 14:02:04 +0300

    Finished BookDetailsFragment Design.

--- app/src/main/java/com/adzumi/thelookbook/ui/BookDetailsFragment.java
+++ app/src/main/java/com/adzumi/thelookbook/ui/BookDetailsFragment.java
@@ -7,12 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.adzumi.thelookbook.R;
 import com.adzumi.thelookbook.models.Work.Work;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +29,9 @@ public class BookDetailsFragment extends Fragment {
     @BindView(R.id.bookTitleDetail) TextView mBookTitleDetail;
     @BindView(R.id.bookDescriptionDetail) TextView mBookDescriptionDetail;
     @BindView(R.id.bookAuthorName) TextView mBookAuthorName;
+    @BindView(R.id.ratingBar2) RatingBar mBookRatingBar;
+    @BindView(R.id.rateTextView) TextView mRateTextView;
+    @BindView(R.id.ratingsTextView) TextView mRatingsTextView;
 
     private Work mWork;
 
@@ -46,9 +54,14 @@ public class BookDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_book_details, container, false);
         ButterKnife.bind(this, view);
 
-//        Picasso.with(view.getContext()).load(mWork.getBestBook().getImageUrl()).into(mBookImageDetail);
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US);
+        double bookRatings = Double.parseDouble(mWork.getRatingsCount().getText());
+        Picasso.with(view.getContext()).load(mWork.getBestBook().getImageUrl()).fit().into(mBookImageDetail);
         mBookTitleDetail.setText(mWork.getBestBook().getTitle());
-        mBookAuthorName.setText(mWork.getBestBook().getAuthor().getName());
+        mBookAuthorName.setText("By " + mWork.getBestBook().getAuthor().getName());
+        mBookRatingBar.setRating(Float.valueOf(mWork.getAverageRating()));
+        mRateTextView.setText(mWork.getAverageRating());
+        mRatingsTextView.setText("(" + formatter.format(bookRatings) + " Ratings)");
 
         return view;
     }
