a89102970ffb3ed9f7f6724e8c79c69eb1d7d58e
yomZsamora <samora.y@adzumi.co.ke>
2018-06-30 08:47:38 +0300

    Added SavedBooksActivity.

---
+++ app/src/main/java/com/adzumi/thelookbook/ui/SavedBooksActivity.java
@@ -0,0 +1,13 @@
+package com.adzumi.thelookbook.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_books);
+    }
+}
